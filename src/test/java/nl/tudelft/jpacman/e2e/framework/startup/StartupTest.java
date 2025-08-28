348530cdcefe6cd70be398f00cabbc01aa879130
LuisBuilds11 <luismipolo11@gmail.com>
2025-08-28 18:27:15 -0500

    Update StartupTest.java

--- src/test/java/nl/tudelft/jpacman/e2e/framework/startup/StartupTest.java
+++ src/test/java/nl/tudelft/jpacman/e2e/framework/startup/StartupTest.java
@@ -1,25 +1,15 @@
 package nl.tudelft.jpacman.e2e.framework.startup;
 
+import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-import io.cucumber.SnippetType;
-import org.junit.runner.RunWith;
-
-/**
- * Entry point for running the the Cucumber tests in JUnit.
- *
- * @author Jan-Willem Gmelig Meyling
- */
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    plugin = {"pretty"},
-    glue = {"nl.tudelft.jpacman.e2e.framework.startup"},
-    features = "classpath:frameworkfeatures/startup")
+    features = "src/test/resources/features",
+    glue = "nl.tudelft.jpacman.e2e.framework.startup"
+)
 public class StartupTest {
-
-    /*
-     * This class should be empty, step definitions should be in separate classes.
-     */
-
+    // No additional code needed here; Cucumber runner picks up feature files
 }
+
